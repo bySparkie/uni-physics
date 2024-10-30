82ac9ab715abe3a5294358341532bb992e485e35
Jan K <jkienast.at@gmail.com>
2024-10-30 22:53:40 +0100

    new version bicylce

--- bicycle.py
+++ bicycle.py
@@ -5,15 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # initialization of values; user input
-v_0 = 30 # starting velocity in km/h
-m = 75 # mass in kg
-c = 0.5 # drag coefficient
+velocity_0 = 30 # starting velocity in km/h
+mass = 75 # in kg
+coefficient_drag = 0.5 # drag coefficient
 
 # initialization of differential equation, given
 def v(t):
-    return m/(c*t+(m/v_0))
+    return (mass*velocity_0)/(coefficient_drag*t*velocity_0+mass)
 
-t_list = np.arange(0, 100, .1)
+t_list = np.linspace(0, 100, 100)
 
 v_list = v(t_list)
 
@@ -23,6 +23,7 @@ plt.plot(t_list, v_list, color='orange')
 plt.xlabel('$t$ in s')
 plt.ylabel('$v$ in km/h')
 plt.title('Velocity of bicycle experiencing drag')
+plt.grid(True)
 
 plt.savefig('plots_pngs/Bicycle_drag.png') # uncomment to save, maybe edit file path
 
